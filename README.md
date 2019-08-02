33382af086ab8f5b5e9379bca8be26f7f85b9785
mzinchenko <mzinchenko@custis.ru>
2019-08-02 20:51:47 +0300

    improvements

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # code-converter
-Code converter for habr.com
+Clipboard code converter for http://habr.com.
+
+Transforms code from usual html IntelliJ IDEA format into special _habr.com_ format.  
+
+**Usage:**
+* Select code region in IntelliJ IDEA
+* Press Ctrl-C (Copy)
+* Run code-converter.jar
+* Press Ctrl-V (Paste)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,10 @@
-package habracode.main
+package com.habr.codeconverter
 
 import java.awt.Toolkit
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.StringSelection
 import java.io.InputStream
 
-val TEXT_FLAVOR = DataFlavor("text/plain", "Rich Formatted Text")
-val RTF_FLAVOR = DataFlavor("text/rtf", "Rich Formatted Text")
 val HTML_FLAVOR = DataFlavor("text/html", "Rich Formatted Text")
 
 val pre = Regex("<pre .*?>(.*?)</pre>")
@@ -16,34 +14,39 @@ val styleItalic = Regex("(font-style:italic)")
 val styleBold = Regex("(font-weight:bold)")
 
 fun main() {
-    val cb = Toolkit.getDefaultToolkit().systemClipboard
-    val source = (cb.getData(HTML_FLAVOR) as InputStream).reader().readText()
-    println(source)
-    val selection = StringSelection(source.substring(pre)!!
+  val cb = Toolkit.getDefaultToolkit().systemClipboard
+  val source = (cb.getData(HTML_FLAVOR) as InputStream).reader().readText()
+  println(source)
+  StringSelection(
+    "<code>" +
+      source.substring(pre)!!
         .replace(span) {
-            it.groups[2]!!.value.styledWith(it.groups[1]!!.value)
-        })
-    cb.setContents(selection, selection)
+          it.groups[2]!!.value.styledWith(it.groups[1]!!.value)
+        }
+        .replace("<br>", "\n")
+        .replace("&#32;", "&nbsp;")
+      + "</code>"
+  ).let { cb.setContents(it, it) }
 }
 
 private fun String.styledWith(style: String) =
-    styledWith(
-        style.substring(color),
-        style.substring(styleItalic) != null,
-        style.substring(styleBold) != null
-    )
+  styledWith(
+    style.substring(color),
+    style.substring(styleItalic) != null,
+    style.substring(styleBold) != null
+  )
 
 private fun String.styledWith(color: String?, italic: Boolean, bold: Boolean) =
-    (color?.let { "<font color=\"$it\">" } ?: "") +
-            (if (italic) "<i>" else "") +
-            (if (bold) "<b>" else "") +
-            this +
-            (if (bold) "</b>" else "") +
-            (if (italic) "</i>" else "") +
-            (color?.let { "</font>" } ?: "")
+  (color?.let { "<font color=\"$it\">" } ?: "") +
+    (if (italic) "<i>" else "") +
+    (if (bold) "<b>" else "") +
+    this +
+    (if (bold) "</b>" else "") +
+    (if (italic) "</i>" else "") +
+    (color?.let { "</font>" } ?: "")
 
 
 private fun String.substring(pattern: Regex) =
-    pattern.find(this)?.let { it.groups[1]?.value }
+  pattern.find(this)?.let { it.groups[1]?.value }
 
 
